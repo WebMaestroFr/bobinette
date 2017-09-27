2fba9eae0f7470d2f311f9486fc48738686930b6
Etienne Baudry <etienne@webmaestro.fr>
2017-09-27 14:04:13 +0200

    Get OpenCV build with `pip install opencv-contrib-python

--- install.sh
+++ install.sh
@@ -28,28 +28,31 @@ git submodule update
 printf "\n${BLUE}Installing Server Requirements ...${BLANK}\n"
 sudo -H pip install -U -r requirements.txt
 
-printf "\n${BLUE}Building OpenCV ...${BLANK}\n"
-cd libraries/opencv_build
-cmake \
--D BUILD_opencv_java=OFF \
--D BUILD_PYTHON_SUPPORT=ON \
--D CMAKE_BUILD_TYPE=RELEASE \
--D CMAKE_INSTALL_PREFIX=/usr/local \
--D INSTALL_PYTHON_EXAMPLES=ON \
--D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
--D WITH_CUDA=OFF \
--D WITH_OPENGL=ON \
--D WITH_TBB=ON \
--D WITH_V4L=ON \
-../opencv
-make -j$(nproc)
-sudo make install
-cd ../..
+# printf "\n${BLUE}Building OpenCV ...${BLANK}\n"
+# cd libraries/opencv_build
+# cmake \
+# -D BUILD_opencv_java=OFF \
+# -D BUILD_PYTHON_SUPPORT=ON \
+# -D CMAKE_BUILD_TYPE=RELEASE \
+# -D CMAKE_INSTALL_PREFIX=/usr/local \
+# -D INSTALL_PYTHON_EXAMPLES=ON \
+# -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+# -D WITH_CUDA=OFF \
+# -D WITH_OPENGL=ON \
+# -D WITH_TBB=ON \
+# -D WITH_V4L=ON \
+# ../opencv
+# make -j$(nproc)
+# sudo make install
+# cd ../..
 
 # printf "\n${BLUE}Installing Node ...${BLANK}\n"
 # wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.9.1.sh | bash
 # curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 # sudo apt-get install -y nodejs
+
+printf "\n${BLUE}Updating Node Package Manager ...${BLANK}\n"
+npm update -g
 
 printf "\n${BLUE}Installing Client Application ...${BLANK}\n"
 cd application
