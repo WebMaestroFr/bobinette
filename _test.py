1e062108894dc53938240ebaf593402610c4df43
Etienne Baudry <etienne@webmaestro.fr>
2017-09-27 15:57:01 +0200

    Test db create

--- _test.py
+++ _test.py
@@ -3,6 +3,7 @@ import sys
 
 import cv2
 import fake_rpi
+from bobinette.__main__ import app, db
 
 sys.modules['RPi'] = fake_rpi.RPi
 sys.modules['picamera'] = fake_rpi.picamera
@@ -11,3 +12,9 @@ sys.modules['picamera'] = fake_rpi.picamera
 def test_opencv_3():
     '''OpenCV Version 3'''
     assert cv2.__version__.startswith('3.')
+
+
+def test_db_create_all():
+    '''Database Creation'''
+    with app.app_context():
+        db.create_all()
