0dd11853f8a660ca7d7df93c332cdf4c25eed9f0
Etienne Baudry <etienne@webmaestro.fr>
2017-09-27 15:28:08 +0200

    Travis OpenCV

--- _test.py
+++ _test.py
@@ -2,7 +2,6 @@
 import cv2
 
 
-def test_pytest():
-    '''Pytest'''
-    print "OpenCV Version : %s" % cv2.__version__
-    assert True
+def test_opencv_3():
+    '''OpenCV Version 3'''
+    assert cv2.__version__.startswith('3.')
