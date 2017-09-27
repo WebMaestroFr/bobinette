1375ed51307dacd1c3f0c66fea631d14ff102a35
Etienne Baudry <etienne@webmaestro.fr>
2017-09-27 14:26:59 +0200

    Remove Submodules

--- install.sh
+++ install.sh
@@ -8,48 +8,19 @@ BLANK="\033[0m"
 printf "\n${BLUE}Updating Package Manager ...${BLANK}\n"
 sudo apt-get update -y
 printf "\n${BLUE}Upgrading Distribution ...${BLANK}\n"
-# sudo apt-get upgrade -y -q
 sudo apt-get dist-upgrade -y -q
+printf "\n${BLUE}Upgrading Packages ...${BLANK}\n"
+sudo apt-get upgrade -y -q
 
 printf "\n${BLUE}Installing Dependencies ...${BLANK}\n"
-sudo apt-get install -y build-essential
-sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-dev python-numpy python-pip
+sudo apt-get install -y python-dev python-pip
 
 printf "\n${BLUE}Cleaning Up Packages ...${BLANK}\n"
 sudo apt-get autoremove -y
 sudo apt-get clean -y
 
-printf "\n${BLUE}Initializing Submodules ...${BLANK}\n"
-git submodule init
-printf "\n${BLUE}Updating Submodules ...${BLANK}\n"
-git submodule update
-
 printf "\n${BLUE}Installing Server Requirements ...${BLANK}\n"
 sudo -H pip install -U -r requirements.txt
-
-# printf "\n${BLUE}Building OpenCV ...${BLANK}\n"
-# cd libraries/opencv_build
-# cmake \
-# -D BUILD_opencv_java=OFF \
-# -D BUILD_PYTHON_SUPPORT=ON \
-# -D CMAKE_BUILD_TYPE=RELEASE \
-# -D CMAKE_INSTALL_PREFIX=/usr/local \
-# -D INSTALL_PYTHON_EXAMPLES=ON \
-# -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
-# -D WITH_CUDA=OFF \
-# -D WITH_OPENGL=ON \
-# -D WITH_TBB=ON \
-# -D WITH_V4L=ON \
-# ../opencv
-# make -j$(nproc)
-# sudo make install
-# cd ../..
-
-# printf "\n${BLUE}Installing Node ...${BLANK}\n"
-# wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.9.1.sh | bash
-# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-# sudo apt-get install -y nodejs
 
 printf "\n${BLUE}Updating Node Package Manager ...${BLANK}\n"
 npm update -g
