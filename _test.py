be6bce31ae8fa084fad88accb6079edc933ccdda
Etienne Baudry <etienne@webmaestro.fr>
2017-09-27 16:04:27 +0200

    Fix tests imports

--- _test.py
+++ _test.py
@@ -1,9 +1,7 @@
 '''Pytest'''
 import sys
 
-import cv2
 import fake_rpi
-from bobinette.__main__ import app, db
 
 sys.modules['RPi'] = fake_rpi.RPi
 sys.modules['picamera'] = fake_rpi.picamera
@@ -11,10 +9,12 @@ sys.modules['picamera'] = fake_rpi.picamera
 
 def test_opencv_3():
     '''OpenCV Version 3'''
+    import cv2
     assert cv2.__version__.startswith('3.')
 
 
 def test_db_create_all():
     '''Database Creation'''
+    from bobinette.__main__ import app, db
     with app.app_context():
         db.create_all()
